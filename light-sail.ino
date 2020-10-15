605f86c1166ff7fe2c34c1b2a036aa2d40dbf2ab
Ron Hodgson <ron.hodgson@me.com>
2020-10-14 20:00:29 -0700

    ramp-up color change

--- light-sail.ino
+++ light-sail.ino
@@ -1,42 +1,39 @@
 #include <Adafruit_NeoPixel.h>
 
 #define NEO_PIXEL_PIN 12
-#define NEO_PIXEL_LENGTH 101
+#define NEO_PIXEL_LENGTH 100
+#define COLOR_LEVEL_MAX 151
+#define COLOR_LEVEL_STEP 10
 
-Adafruit_NeoPixel pixels(NEO_PIXEL_LENGTH, NEO_PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(NEO_PIXEL_LENGTH, NEO_PIXEL_PIN, NEO_RGB + NEO_KHZ800);
 
 void setup() {
+    Serial.begin(115200);
     pixels.begin();
 }
 
+int red = 0;
+int green = 0xff;
+int blue = 0;
+
 void loop() {
-    pixels.clear();
+    int old_red = red;
+    red = green;
+    green = blue;
+    blue = old_red;
 
-    for (int i = 0; i < NEO_PIXEL_LENGTH; i++) {
-        pixels.setPixelColor(i, pixels.Color(150, 0, 0));
-    }
-
-    pixels.show();
-
-    delay(500);
-
-    pixels.clear();
-
-    for (int i = 0; i < NEO_PIXEL_LENGTH; i++) {
-        pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-    }
-
-    pixels.show();
-
-    delay(500);
+    Serial.print("Red = "); Serial.print(red, HEX);
+    Serial.print(", Green = "); Serial.print(green, HEX);
+    Serial.print(", Blue = "); Serial.println(blue, HEX);
 
     pixels.clear();
-
-    for (int i = 0; i < NEO_PIXEL_LENGTH; i++) {
-        pixels.setPixelColor(i, pixels.Color(0, 0, 150));
+    for (int c = 0; c < COLOR_LEVEL_MAX; c += COLOR_LEVEL_STEP ) {
+        for (int i = 0; i < NEO_PIXEL_LENGTH; i++) {
+            pixels.setPixelColor(i, pixels.Color(c & red, c & green, c & blue));
+        }
+        pixels.show();
+        delay(100);
     }
-
-    pixels.show();
 
     delay(500);
 }
